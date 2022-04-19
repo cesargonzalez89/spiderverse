d6b907686c83eede92f251c5f62a286949a03a16
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-19 00:32:11 -0500

    Agregando una segunda prueba de unidad

--- app/spiderman.js
+++ app/spiderman.js
@@ -6,5 +6,9 @@ class Spiderman{
         this.movies = movies
         this.studio = studio
     }
+
+    getInfo(){
+        return `Hey, I´m ${this.actor} from ${this.studio} Studio`
+    }
 }
 module.exports = Spiderman
